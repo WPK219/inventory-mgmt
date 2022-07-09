40e1f691486f616c6228d9e1faa33c4131856d04
WPK219 <waiphyokyaw.yumyum@gmail.com>
2022-07-09 13:14:57 +0630

    Add Supplier Phnos related files

---
+++ app/Http/Controllers/SupplierPhnoController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreSupplierPhnosRequest;
+use App\Http\Requests\UpdateSupplierPhnosRequest;
+use App\Models\SupplierPhnos;
+
+class SupplierPhnosController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreSupplierPhnosRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreSupplierPhnosRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\SupplierPhnos  $supplierPhnos
+     * @return \Illuminate\Http\Response
+     */
+    public function show(SupplierPhnos $supplierPhnos)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\SupplierPhnos  $supplierPhnos
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(SupplierPhnos $supplierPhnos)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateSupplierPhnosRequest  $request
+     * @param  \App\Models\SupplierPhnos  $supplierPhnos
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateSupplierPhnosRequest $request, SupplierPhnos $supplierPhnos)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\SupplierPhnos  $supplierPhnos
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(SupplierPhnos $supplierPhnos)
+    {
+        //
+    }
+}
